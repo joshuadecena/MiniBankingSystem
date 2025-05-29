969ab16aa1995371f8b8ec5f8e5fdec79fcd95dc
chymken <joshuaxdecena@gmail.com>
2025-05-29 14:10:47 +0800

    Update Account.java

--- mbs/src/main/java/com/capstone/mbs/entity/Account.java
+++ mbs/src/main/java/com/capstone/mbs/entity/Account.java
@@ -1,1 +1,72 @@
 package com.capstone.mbs.entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.DecimalMin;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
+
+public class Account {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long accountId;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+	private User user;
+	
+	@Column(nullable = false, length = 100)
+	@NotBlank
+	private String name;
+	
+	@Column(length = 100, nullable = false, unique = true)
+	@Email
+	private String email;
+	
+	@Column(length = 255)
+	private String address;
+	
+	@Column(name = "date_of_birth", nullable = false)
+	private LocalDate dateOfBirth;
+	
+	@Column(nullable = false)
+	@DecimalMin(value = "0.0", inclusive = false)
+	private BigDecimal balance;
+	
+	//Constructors
+	public Account () {
+		// Default constructor
+	}
+	
+	public Account(User user, String name, String email, String address, LocalDate dateOfBirth, BigDecimal balance) {
+		this.user = user;
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.dateOfBirth = dateOfBirth;
+		this.balance = balance;
+	}
+	
+}
