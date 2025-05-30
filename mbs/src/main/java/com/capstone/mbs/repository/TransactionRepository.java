800274fa0d5c0a7132ba82deb60683c6ae625518
gelomalonzo <malonzoab@gmail.com>
2025-05-30 16:21:49 +0800

    style: removed comments

--- mbs/src/main/java/com/capstone/mbs/repository/TransactionRepository.java
+++ mbs/src/main/java/com/capstone/mbs/repository/TransactionRepository.java
@@ -8,36 +8,7 @@ import com.capstone.mbs.entity.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 	
-	/*
-	 * Get all transactions (history) of specific account.
-	 * 
-	 * USE: For displaying transaction history of a Customer on that Customer's dashboard.
-	 */
+	// transaction history of specific account
 	List<Transaction> findBySourceAccountAccountIdOrDestinationAccountAccountId(Long accountId);
-	
-//	/*
-//	 * Filter by Transaction Type and Timestamp.
-//	 * 
-//	 * USE: For filtering transaction history as Admin.
-//	 */
-//	List<Transaction> findByTransactionTypeContainsAndTimestampBetween(
-//			String transactionType,
-//			LocalDateTime minDate,
-//			LocalDateTime maxDate,
-//			Sort sort
-//	);
-	
-//	/*
-//	 * Filter by Account ID, Transaction Type, and Timestamp.
-//	 * 
-//	 * USE: For filtering transaction history as Admin or Customer.
-//	 */
-//	List<Transaction> findBySourceAccountAccountIdOrDestinationAccountAccountIdAndTransactionTypeContainsAndTimestampBetween(
-//			Long accountId,
-//			String transactionType,
-//			LocalDateTime minDate,
-//			LocalDateTime maxDate,
-//			Sort sort
-//	);
 
 }
--- mbs/src/main/java/com/capstone/mbs/service/TransactionService.java
+++ mbs/src/main/java/com/capstone/mbs/service/TransactionService.java
@@ -2,27 +2,13 @@ package com.capstone.mbs.service;
 
 import java.util.List;
 
-import com.capstone.mbs.dto.TransactionDTO;
+import com.capstone.mbs.dto.TransactionCreateDTO;
+import com.capstone.mbs.dto.TransactionResponseDTO;
 
 public interface TransactionService {
 	
-	/*
-	 * Get all of the transactions of all accounts. 
-	 * Calls getAllTransactions(Sort),
-	 * with sorting defaulted to descending timestamp (most recent first).
-	 */
-	List<TransactionDTO> getAllTransactions();
-	
-	/*
-	 * Get all transactions of specific account.
-	 * Calls getAllAccountTransactions(Long, Sort),
-	 * with sorting defaulted to descending timestamp (most recent first).
-	 */
-	List<TransactionDTO> getAllAccountTransactions(Long accountId);
-	
-	/*
-	 * Add a new transaction.
-	 */
-	TransactionDTO createTransaction(TransactionDTO transactionDTO);
+	List<TransactionResponseDTO> getAllTransactions(); // retrieve all transactions from all accounts
+	List<TransactionResponseDTO> getAllAccountTransactions(Long accountId); // retrieve all transactions from a specific account
+	TransactionResponseDTO createTransaction(TransactionCreateDTO transactionCreateDTO);
 
 }
