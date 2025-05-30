bc47d69712c72f170b8ccdfe16f8fb085b990430
chymken <joshuaxdecena@gmail.com>
2025-05-30 15:28:09 +0800

    feat(account) - account repository

--- mbs/src/main/java/com/capstone/mbs/repository/AccountRepository.java
+++ mbs/src/main/java/com/capstone/mbs/repository/AccountRepository.java
@@ -14,12 +14,13 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 	// Find an account by its ID since it is one to one(One User = One Account)
 	Optional<Account> findByUserUserId(Long userId);
 	
+	// Find by name(if name is not unique, return list), case-insensitive
+	List<Account> findByNameIgnoreCase(String name);
+	
 	// Find by account ID
 	Optional<Account> findByAccountId(Long accountId);
 	
 	// Find by email(must be unique), case-insensitive
 	Optional<Account> findByEmailIgnoreCase(String email);
 	
-	// Find by name(if name is not unique, return list), case-insensitive
-	List<Account> findByNameIgnoreCase(String name);
 }
